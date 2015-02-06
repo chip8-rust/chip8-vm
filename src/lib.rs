1521794ba08d634e7daa0fc38fbd398ed04d9c56
robo9k <robo@9k.lv>
2015-02-06 14:09:49 +0100

    Silence some warnings for `chip8_vm`

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+// Silence 'core' feature warnings
+// for `error:Error` and such
+#![feature(core)]
+// Silence 'io' feature warnings
+// for `BufWriter` and such
+#![feature(io)]
+
 pub mod error;
 pub mod ops;
 pub mod vm;

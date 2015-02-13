2995e20740089f76ec62a4fcbd8fb893239f0990
robo9k <robo@9k.lv>
2015-02-13 20:47:35 +0100

    Add rustdoc for `Instruction` variants

--- src/instructions.rs
+++ src/instructions.rs
@@ -132,41 +132,103 @@ impl RawInstruction {
 /// A valid instruction that can be executed as-is.
 #[derive(Copy,Debug)]
 pub enum Instruction {
+    /// Jumps to machine subroutine at `Addr`.
+    ///
+    /// Note that this is intentionally not implemented in the `Vm`.
     Sys(Addr),              // 0nnn - SYS addr
+    /// Clears the screen.
+    ///
+    /// Sets all pixels to the unlit state.
     Clear,                  // 00E0 - CLS
+    /// Returns from a subroutine
     Return,                 // 00EE - RET
+    /// Jumps to `Addr`
     Jump(Addr),             // 1nnn - JP addr
+    /// Calls subroutine at `Addr`
     Call(Addr),             // 2nnn - CALL addr
+    /// Skips the next instructions if `Vx` equals `Byte`
     SkipEqualK(Vx, Byte),   // 3xkk - SE Vx, byte
+    /// Skips the next instruction if `Vx` does not equal `Byte`
     SkipNotEqualK(Vx, Byte),// 4xkk - SNE Vx, byte
+    /// Skips the next instruction if `Vx` and `Vy` are equal
     SkipEqual(Vx, Vy),      // 5xy0 - SE Vx, Vy
+    /// Sets `Vy` to `Byte`
     SetK(Vx, Byte),         // 6xkk - LD Vx, byte
+    /// Adds `Byte` to `Vx`, then stores the result in `Vx`
     AddK(Vx, Byte),         // 7xkk - ADD Vx, byte
+    /// Stores `Vy` in `Vx`
     Set(Vx, Vy),            // 8xy0 - LD Vx, Vy
+    /// Performs a bitwise OR (`|`) of `Vx` and `Vy`, then stores the result in `Vx`
     Or(Vx, Vy),             // 8xy1 - OR Vx, Vy
+    /// Performs a bitwise AND (`&`) of `Vx` and `Vy`, then stores the result in `Vx`
     And(Vx, Vy),            // 8xy2 - AND Vx, Vy
+    /// Performs a bitwise XOR (`^`) of `Vx` and `Vy`, then stores the result in `Vx`
     XOr(Vx, Vy),            // 8xy3 - XOR Vx, Vy
+    /// Adds `Vx` and `Vy`, then stores the result in in `Vx`.
+    ///
+    /// `VF` is set to `1` on overflow, `0` otherwise.
     Add(Vx, Vy),            // 8xy4 - ADD Vx, Vy
+    /// Subtracts `Vy` from `Vx`, then stores the result in `Vx`.
+    ///
+    /// `VF` is set to `1` if `Vx` is larger than `Vy` prior subtraction, `0` otherwise.
     Sub(Vx, Vy),            // 8xy5 - SUB Vx, Vy
+    /// Shifts `Vy` right by one bit, then stores the result in `Vx`.
+    ///
+    /// Stores the least-significant bit prior shift of `Vy` in `VF`.
     ShiftRight(Vx, Vy),     // 8xy6 - SHR Vx {, Vy}
+    /// Subtracts `Vx` from `Vy`, then stores the result in `Vx`.
+    ///
+    /// `VF` is set to `1` if `Vx` is larger than `Vy` prior subtraction, `0` otherwise.
+    ///
+    /// Note that this is the same as `Sub` with inverted register operands.
     SubInv(Vx, Vy),         // 8xy7 - SUBN Vx, Vy
+    /// Shifts `Vy` left by one bit, then stores the result in `Vx`.
+    ///
+    /// Stores the most-significant bit prior shift of `Vy` in `VF`.
     ShiftLeft(Vx, Vy),      // 8xyE - SHL Vx {, Vy}
+    /// Skips the next instruction if `Vx` and `Vy` are not equal
     SkipNotEqual(Vx, Vy),   // 9xy0 - SNE Vx, Vy
+    /// Sets the `I` register to `Addr`
     LoadI(Addr),            // Annn - LD I, addr
+    /// Jumps to `V0 + Addr`
     LongJump(Addr),         // Bnnn - JP V0, addr
+    /// Sets `Vx` to a random byte ANDed with `Byte`
     Rand(Vx, Byte),         // Cxkk - RND Vx, byte
+    /// Draws the sprite with `Nibble` bytes of data from the `I` register at position `(Vx, Vy)`.
+    ///
+    /// Sets `VF` to `1` if any pixels are set to unlit state, `0` otherwise.
+    ///
+    /// Note that sprites wrap around onto the opposite side of the screen.
     Draw(Vx, Vy, Nibble),   // Dxyn - DRW Vx, Vy, nibble
+    /// Skips the next instruction if key `Vx` is pressed
     SkipPressed(Vx),        // Ex9E - SKP Vx
+    /// Skips the next instruction if key `Vx` is not pressed
     SkipNotPressed(Vx),     // ExA1 - SKNP Vx
+    /// Stores the value of the `delay timer` in `Vx`
     GetTimer(Vx),           // Fx07 - LD Vx, DT
+    /// Stops execution until a key is pressed, then stores that key in `Vx`
     WaitKey(Vx),            // Fx0A - LD Vx, K
+    /// Sets the `delay timer` to `Vx`
     SetTimer(Vx),           // Fx15 - LD DT, Vx
+    /// Sets the `sound timer` to `Vx`
     SetSoundTimer(Vx),      // Fx18 - LD ST, Vx
+    /// Adds `Vx` and the `I` register, then stores the result in `I`
     AddToI(Vx),             // Fx1E - ADD I, Vx
+    /// Stores the address of the hexadecimal digit `Vx` in the `I` register
     LoadHexGlyph(Vx),       // Fx29 - LD F, Vx
+    /// Stores the binary-coded decimal representation of `Vx` at address `I`, `I + 1` and `I + 2`
     StoreBCD(Vx),           // Fx33 - LD B, Vx
+    /// Stores the registers `V0` to `Vx` inclusive at address `I`.
+    ///
+    /// Register `I` is set to `I + Vx + 1` afterwards.
     StoreRegisters(Vx),     // Fx55 - LD [I], Vx
+    /// Reads the registers `V0` to `Vx` inclusive from address `I`.
+    ///
+    /// Register `I` is set to `I + Vx + 1` afterwards.
     LoadRegisters(Vx),      // Fx65 - LD Vx, [I]
+    /// Placeholder for an unknown or illegal instruction.
+    ///
+    /// Note that this is not a real CHIP-8 instruction.
     Unknown,
 }
 

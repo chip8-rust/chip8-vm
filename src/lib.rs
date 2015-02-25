3333ac757c2dcd8a50689310520e082520475cf2
robo9k <robo@9k.lv>
2015-02-25 15:43:43 +0100

    Add function to return crate version

--- src/lib.rs
+++ src/lib.rs
@@ -31,3 +31,13 @@ pub mod instructions;
 pub mod vm;
 
 pub use instructions::*;
+
+/// Returns the version of this crate in the format `MAJOR.MINOR.PATCH`.
+pub fn version() -> &'static str {
+    // TODO: There's also an optional _PRE part
+    concat!(
+        env!("CARGO_PKG_VERSION_MAJOR"), ".",
+        env!("CARGO_PKG_VERSION_MINOR"), ".",
+        env!("CARGO_PKG_VERSION_PATCH"),
+    )
+}
